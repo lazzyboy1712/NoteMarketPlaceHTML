5e939486bccefeff16aca277a66779b723666588
Yash Unadkat <61225912+lazzyboy1712@users.noreply.github.com>
2021-03-27 20:48:55 +0530

    Add files via upload

---
+++ Models/ContactUs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace notesmarketplace.Models
+{
+    public class ContactUs
+    {
+        public string FullName { get; set; }
+        public string EmailID { get; set; }
+        public string Subject { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
